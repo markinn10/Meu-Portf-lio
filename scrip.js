595f410d2a277ec398c09b496c253453511b197d
Marcus Vinicius <148286337+markinn10@users.noreply.github.com>
2024-08-29 11:53:46 -0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,15 @@
+<script>
+document.querySelectorAll('.scroll-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+</script>
